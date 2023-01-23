2ff2e5ff601cc45c90e18d16863be32df71750fc
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-23 20:52:57 +0330

    redirecting ability added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\HTTP\Controllers\UserController;
+use App\HTTP\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,3 +18,4 @@ Route::get('/login', function () {
     return view('login');
 });
 Route::post('/login',[UserController::class,'login']);
+Route::get('/',[ProductController::class,'login']);
