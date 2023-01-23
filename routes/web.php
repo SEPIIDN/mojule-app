f98d54e24879606a2caeba065e2691138ecbc5f7
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-23 20:42:52 +0330

    minor changes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\HTTP\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
+Route::post('/login',[UserController::class,'login']);
