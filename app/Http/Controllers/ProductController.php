be34cf1e56d2efd72f9624a57850d0de0737a842
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-24 00:38:57 +0330

    order added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 use Session;
 use Illuminate\Support\Facades\DB;
 
