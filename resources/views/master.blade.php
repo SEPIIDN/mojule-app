63c78067aeeab33a2c2e88bfe81aebc534dbeefe
Sepehr Dehnamaki <sepehrdehnamaki@gmail.com>
2023-01-23 19:58:35 +0330

    built login form and navbar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,6 +13,8 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
-    
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
 </body>
 </html>
